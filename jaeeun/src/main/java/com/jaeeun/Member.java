c2ceab090f81403d15d12fd42e8d1c1c5f5e59b4
JAEEUN SHIN <122805045+JaeeunSkywalker@users.noreply.github.com>
2023-09-18 06:00:51 +0900

    Chore: 5장 끝

--- jaeeun/src/main/java/com/jaeeun/Member.java
+++ jaeeun/src/main/java/com/jaeeun/Member.java
@@ -10,17 +10,17 @@ import lombok.*;
 
 //얘는 MemberDAO입니다.
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // 기본 생성자
 @AllArgsConstructor
 @Getter
-@Entity
+@Entity // 엔티티로 지정
 public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id // id 필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키를 자동으로 1씩 증가
     @Column(name = "id", updatable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false)
+    @Column(name = "name", nullable = false) // name이라는 not null 칼럼과 매핑
     private String name;
 
 }
